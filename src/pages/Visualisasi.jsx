5291a7974fa96a431de0282f88c37653173f69d8
dittdott <aditiyapurwansyah@apps.ipb.ac.id>
2025-05-23 21:07:59 +0700

    nambah

---
+++ src/pages/Visualisasi.jsx
@@ -0,0 +1,8 @@
+export default function Visualisasi() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold mb-4">Visualisasi</h1>
+      <p className="text-gray-700">Learn more about our website and mission.</p>
+    </div>
+  );
+}
