105935b233922837d0b9845560df012067f3a161
dittdott <aditiyapurwansyah@apps.ipb.ac.id>
2025-05-23 18:42:39 +0700

    homereadydikit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,46 @@
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Home Page</h1>
-      <p className="text-gray-700">This is the home page of your website.</p>
-    </div>
+    <section className="flex flex-1 flex-col h-screen py-10 mx-auto">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-3xl font-bold">Beranda</h1>
+      </div>
+      <div className="bg-[#670075] flex justify-evenly rounded-xl items-center">
+        <div className="text-white p-5 gap-y-2 -ml-10">
+          <p className="font-[200] text-sm text-sm">Total sertifikat data</p>
+          <p className="font-bold text-[1.5rem]">12455</p>
+          <p className="font-[200] text-sm text-sm">12 marey 2020</p>
+        </div>
+        <div class="w-[0.5px] h-[60%] bg-white my-5 -translate-x-5"></div>
+        <div className="text-white p-5 gap-y-2 -ml-10">
+          <p className="font-[200] text-sm text-sm ">Sertifikat data</p>
+          <p className="font-bold text-[1.5rem]">12455</p>
+          <p className="font-[200] text-sm">12 marey 2020</p>
+        </div>
+        <div class="w-[0.5px] h-[60%] bg-white my-5"></div>
+        <div className="text-white p-5 gap-y-2 -ml-10">
+          <p className="font-[200] text-sm ">Total sertifikat data</p>
+          <p className="font-bold text-[1.5rem]">12455</p>
+          <p className="font-[200] text-sm">12 marey 2020</p>
+        </div>
+        <div class="w-[0.5px] h-[60%] bg-white my-5"></div>
+        <div className="text-white p-5 gap-y-2 -ml-10">
+          <p className="font-[200] text-sm ">Total sertifikat data</p>
+          <p className="font-bold text-[1.5rem]">12455</p>
+          <p className="font-[200] text-sm">12 marey 2020</p>
+        </div>
+      </div>
+      <div>
+        {/* kontener tengah */}
+        <div>
+        {/* kontener tengah kiri */}
+        </div>
+        <div>
+        {/* kontener tengah kanan */}
+        </div>
+      </div>
+      <div>
+        {/* Kontener bawah */}
+      </div>
+    </section>
   );
 }
