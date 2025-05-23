10d3757e6a8233e5dca08cca2f97f12e4c486346
dittdott <aditiyapurwansyah@apps.ipb.ac.id>
2025-05-23 21:10:11 +0700

    statistikadd

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,8 +1,101 @@
+import { IoIosArrowDown } from "react-icons/io";
+import Barchart from "../components/Barchart";
+import Piechart from "../components/Piechart";
+import MapView from "../components/MapView";
+import { FaCircle } from "react-icons/fa";
+import { VscListFlat } from "react-icons/vsc";
+
 export default function Statistics() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold mb-4">Statistics</h1>
-      <p className="text-gray-700">Learn more about our website and mission.</p>
-    </div>
+    <section className="flex flex-1 flex-col h-screen py-10 mx-auto">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-3xl font-bold flex items-center gap-2"> <VscListFlat /> Beranda</h1>
+      </div>
+
+      {/* atas */}
+      <div className="bg-[#670075] flex justify-evenly rounded-xl items-center">
+        <div className="text-white p-5 gap-y-2 -ml-10">
+          <p className="font-[200] text-sm text-sm">Total sertifikat data</p>
+          <p className="font-bold text-[1.5rem]">12455</p>
+          <p className="font-[200] text-sm text-sm">12 marey 2020</p>
+        </div>
+        <div className="w-[0.5px] h-[60%] bg-white my-5 -translate-x-5"></div>
+        <div className="text-white p-5 gap-y-2 -ml-15">
+          <p className="font-[200] text-sm text-sm ">Sertifikat data</p>
+          <p className="font-bold text-[1.5rem]">2.047</p>
+          <p className="font-[200] text-sm">Per bulan Maret 2025</p>
+        </div>
+        <div className="w-[0.5px] h-[60%] bg-white my-5"></div>
+        <div className="text-white p-5 gap-y-2 -ml-10">
+          <p className="font-[200] text-sm ">Persentase Sertifikat 2025</p>
+          <p className="font-bold text-[1.5rem]">11%</p>
+          <p className="font-[200] text-sm">Naik 34.000 Sertifikat Terdata</p>
+        </div>
+        <div className="w-[0.5px] h-[60%] bg-white my-5"></div>
+        <div className="text-white p-5 gap-y-2 -ml-10">
+          <p className="font-[200] text-sm ">Top Sektor terverifikasi</p>
+          <p className="font-bold text-[1.5rem]">FnB</p>
+          <p className="font-[200] text-sm">985.321 Sertifikat</p>
+        </div>
+      </div>
+
+      {/* Tengah */}
+      <div className="flex justify-evenly mt-10 gap-x-5">
+        <div className="border-2 border-gray-300 p-5 rounded-lg shadow-lg flex-1 flex flex-col items-center justify-between">
+          <div className="flex flex-row justify-between w-full border-b border-gray-300">
+            <div>
+              <p className="text-sm text-gray-500">Sertifikat</p>
+              <p className="font-bold text-lg">Statistik Time Series</p>
+            </div>
+            <div className="bg-[#670075] rounded-2xl p-2 h-fit">
+             <p className="text-xs text-gray-500 text-white flex items-center gap-x-3">Tahun <IoIosArrowDown /></p>
+            </div>
+          </div>
+          <div className="w-full">
+            <Barchart />
+          </div>
+        </div>
+
+        <div className="border-2 border-gray-300 p-5 rounded-lg shadow-lg flex-1 flex flex-col items-center justify-between">
+          <div className="flex flex-row justify-between w-full border-b border-gray-300">
+            <div>
+              <p className="text-sm text-gray-500">Sertifikat</p>
+              <p className="font-bold text-lg">Statistik Time Series</p>
+            </div>
+            <div className="bg-[#670075] rounded-2xl p-2 h-fit">
+             <p className="text-xs text-gray-500 text-white flex items-center gap-x-3">Tahun <IoIosArrowDown /></p>
+            </div>
+          </div>
+          <div className="w-full">
+            <Piechart />
+          </div>
+        </div>
+      </div>
+
+      {/* Bawah */}
+      <div className="flex justify-evenly mt-10 gap-x-5">
+        <div className="border-2 border-gray-300 p-5 rounded-lg shadow-lg flex-1 flex flex-col items-center justify-between">
+          <div className="flex flex-row w-full">
+            <div className="flex flex-col">
+              <div className="h-fit">
+                <p className="text-sm text-gray-500">Sertifikat</p>
+                <p className="font-bold text-lg">Berdasarkan Wilayah</p>
+                <p className="text-sm text-gray-500">Keterangan</p>
+              </div>
+              <div className="ml-5">
+                <p className="font-bold text-md flex items-center"> <FaCircle className="w-2 mr-2"/> FnB</p>
+                <p className="font-bold text-md flex items-center"> <FaCircle className="w-2 mr-2" /> Kosmetik</p>
+                <p className="font-bold text-md flex items-center"> <FaCircle className="w-2 mr-2"/> Obat</p>
+                <p className="font-bold text-md flex items-center"> <FaCircle className="w-2 mr-2"/> Tekstil</p>
+                <p className="font-bold text-md flex items-center"> <FaCircle className="w-2 mr-2"/> Lainnya</p>
+              </div>
+            </div>
+             <div className="w-full">
+                <MapView />
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
