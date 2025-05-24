ca8b33bc6dca276779353fc6ca1c1b36e5948b7a
dittdott <aditiyapurwansyah@apps.ipb.ac.id>
2025-05-24 09:54:23 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import Home from "./pages/Home.jsx";
 import Statistics from "./pages/Statistics.jsx";
 import Geospatial from "./pages/Geospatial.jsx";
+import Visualisasi from "./pages/Visualisasi.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,7 +17,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="statistics" element={<Statistics />} />
           <Route path="geospatial" element={<Geospatial />} />
           <Route path="infographics" element={<Home />} />
-          <Route path="prediction" element={<Home />} />
+          <Route path="prediction" element={<Visualisasi />} />
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
