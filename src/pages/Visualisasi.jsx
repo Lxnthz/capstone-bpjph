e63c8a585dbcdc3581517b0bcc1d9e028d5f1f33
dittdott <aditiyapurwansyah@apps.ipb.ac.id>
2025-06-25 18:32:06 +0700

    dropdownvisualisasi

--- src/pages/Visualisasi.jsx
+++ src/pages/Visualisasi.jsx
@@ -1,13 +1,10 @@
 import { VscListFlat } from "react-icons/vsc";
-import DropDownBar from "../components/DropDownBar";
-import DropDownPie from "../components/DropDownPie";
 import Barchart from "../components/BarchartHome";
-import Piechart from "../components/Piecharthome"; 
+import DropDown from "../components/DropDownVisualisasi";
 import { useState } from "react";
 
 export default function Visualisasi() {
   const [barYear, setBarYear] = useState("Tahun");
-  const [pieYear, setPieYear] = useState("Tahun");
 
   return (
     <section className="flex flex-1 flex-col h-screen py-10 mx-auto">
@@ -19,14 +16,14 @@ export default function Visualisasi() {
 
       {/* Tengah */}
       <div className="flex justify-evenly mt-10 gap-x-5">
-        {/* Bar Chart */}
+        {/* Bar Kiri */}
         <div className="border-2 border-gray-300 p-5 rounded-lg shadow-lg flex-1 flex flex-col items-center justify-between">
           <div className="flex flex-row justify-between w-full border-b border-gray-300">
             <div>
               <p className="text-sm text-gray-500">Histogram</p>
               <p className="font-bold text-lg">Prediksi Per Jenis Produk</p>
             </div>
-            <div className="flex justify-between items-center">
+            <div className="">
               <p className="text-xs text-white bg-[#670075] rounded-xl px-2 py-2 w-fit">13 Juni 2025</p>
             </div>
           </div>
@@ -35,15 +32,15 @@ export default function Visualisasi() {
           </div>
         </div>
       
-        {/* Pie Chart */}
+        {/* Bar Kanan*/}
         <div className="border-2 border-gray-300 p-5 rounded-lg shadow-lg flex-1 flex flex-col items-center justify-between">
           <div className="flex flex-row justify-between w-full border-b border-gray-300">
             <div>
               <p className="text-sm text-gray-500">Histogram</p>
               <p className="font-bold text-lg">Prediksi Kab/Kota(Top 8)</p>
             </div>
-            <div className="flex justify-between items-center">
-              <p className="text-xs text-white bg-[#670075] rounded-xl px-2 py-2 w-fit">2025</p>
+            <div className="">
+              <DropDown selectedYear={barYear} onChange={setBarYear} />
             </div>
           </div>
           <div className="w-full">
